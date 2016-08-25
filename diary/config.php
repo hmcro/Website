6ff2905a397d53265df341307f8c53b5ba4a901c
Luke Sturgeon <hello@lukesturgeon.co.uk>
2016-08-25 12:35:12 +0100

    removed diary from live site

--- diary/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'hmcro_test');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-
-?>
--- diary/diary_reset.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// use this to clear the cookie and reset things
-unset( $_COOKIE['CID'] );
-setcookie('CID', '', time() - 3600);
-
-header('Location: /diary');
-
-?>
