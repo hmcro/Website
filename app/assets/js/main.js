036f023aa688ab276fb874dcb8d94e3af13fb804
Luke Sturgeon <hello@lukesturgeon.co.uk>
2016-08-25 12:35:00 +0100

    added app page to repo

---
+++ app/assets/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function (){
+  "use strict";
+
+  // do stuff when the page is ready
+
+});
---
+++ app/assets/js/min/main-min.js
@@ -0,0 +1,8 @@
+$(document).ready(function (){
+  "use strict";
+
+  // do stuff when the page is ready
+
+});
+
+
